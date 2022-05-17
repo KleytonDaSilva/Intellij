24dd86075f0f1d2330c8d15c754eeab67d01a80c
KleytonDaSilva <nksaddam@gmail.com>
2022-05-17 10:53:03 -0300

    Atualização dia 17/05/2022- estrutura de dados

--- src/curso/java/estrutura/loiane/aulaLista/ExerciciosEstrutura/Main.java
+++ src/curso/java/estrutura/loiane/aulaLista/ExerciciosEstrutura/Main.java
@@ -1,18 +1,18 @@
 package curso.java.estrutura.loiane.aulaLista.ExerciciosEstrutura;
 
+import curso.java.estrutura.loiane.aulaLista.Estrutura_dados_lista;
+
 import javax.xml.transform.Source;
 
 //Exercicio01 - Melhore a classe Lista e implemente o método contém, semelhando ao contains da classe ArrayList
 public class Main {
     public static void main(String[] args) {
-        Nodo<String> lista = new Nodo<String>(5);
+        ExercicioEstruturaDeDados lista =  new ExercicioEstruturaDeDados(5);
 
         lista.adiciona("A");
         lista.adiciona("B");
         lista.adiciona("C");
 
-        System.out.println(lista.contem("A"));
-        System.out.println(lista.contem("B"));
-        System.out.println(lista.contem("C"));
+
     }
 }
