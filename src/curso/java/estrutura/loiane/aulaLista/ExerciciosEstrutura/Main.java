27f0d657ff48459d9589db0d16a3fa447699cd5b
Kleyton <nksaddam@gmail.com>
2022-05-17 14:36:22 -0300

    Atualização-17/05/2022 Correções de exercicios

--- src/curso/java/estrutura/loiane/aulaLista/ExerciciosEstrutura/Main.java
+++ src/curso/java/estrutura/loiane/aulaLista/ExerciciosEstrutura/Main.java
@@ -2,17 +2,43 @@ package curso.java.estrutura.loiane.aulaLista.ExerciciosEstrutura;
 
 import curso.java.estrutura.loiane.aulaLista.Estrutura_dados_lista;
 
+import java.util.ArrayList;
+
+import javax.sound.sampled.SourceDataLine;
 import javax.xml.transform.Source;
 
-//Exercicio01 - Melhore a classe Lista e implemente o método contém, semelhando ao contains da classe ArrayList
+
 public class Main {
     public static void main(String[] args) {
-        ExercicioEstruturaDeDados lista =  new ExercicioEstruturaDeDados(5);
+
+        //Exercicio01 - Melhore a classe Lista e implemente o método contém, semelhando ao contains da classe ArrayList
+        ExercicioEstruturaDeDados lista = new ExercicioEstruturaDeDados(5);
 
         lista.adiciona("A");
         lista.adiciona("B");
-        lista.adiciona("C");
+        lista.adiciona("A");
 
+        System.out.println(lista.contem("A"));
+        System.out.println(lista.contem("B"));
+        System.out.println(lista.contem("E"));
 
-    }
+ //-----------------------------------------------------------------------------------------
+    System.out.println("-------------------------------------------------------------------------------------");
+        //Exercicio - Melhore a classe Lista e implemente o método ultimoIndioce, semelhante ao método LastindexOF do ArrayList
+        ArrayList<String> arrayList = new ArrayList<String>(5);
+
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("A");
+        
+        
+        System.out.println(arrayList.lastIndexOf("A"));
+        System.out.println("-----------------------------------------------------");
+        System.out.println(lista.ultimoIndice("A"));
+      
+
+    } 
+
+   
+
 }
