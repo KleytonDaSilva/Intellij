15ed60e9d51d6043cccb63eab115beddfd34e113
KleytonDaSilva <nksaddam@gmail.com>
2022-05-17 09:41:11 -0300

    Atualização dia 17/05/2022- estrutura de dados

--- src/faculdade/programacao/estrutura/EstruturaLivroJava/Lista.java
+++ src/faculdade/programacao/estrutura/EstruturaLivroJava/Lista.java
@@ -2,4 +2,40 @@ package faculdade.programacao.estrutura.EstruturaLivroJava;
 
 public class Lista <T>{
 
+    private Node<T> primeiroNo;
+    private Node<T> ultimoNo;
+    private String nome; // String como lista usada na impressão
+
+    //Construtor cria Lista vazia
+
+    public Lista(){
+        this("Lista");
+    }
+
+    public Lista (String listaNome){
+        nome = listaNome;
+        primeiroNo = ultimoNo = null;
+    }
+
+    // insere o item na frente da lista
+    public void InsereNaFrente(T insereItem){
+        if(ehVazio()){// primeiroNo  e ultimoNO referenciam o mesmo objeto
+            primeiroNo =ultimoNo = new Node<T>(insereItem);
+        }else{// primeiroNo referencia o novo nó
+            primeiroNo = new Node<T>(insereItem, primeiroNo);
+        }
+
+    }
+    // insere o item no fim da lista
+    public void InsereNoFim( T insereItem){
+        if(ehVazio()){
+
+        }
+    }
+
+    public boolean ehVazio(){
+
+     return primeiroNo == null;
+    }
+
 }
