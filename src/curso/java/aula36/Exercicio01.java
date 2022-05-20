3e7d258d25c4d0b733f44d2a96f646821bbd5419
Kleyton <nksaddam@gmail.com>
2022-05-20 17:56:56 -0300

    Atualização 20/05/2022 Java

---
+++ src/curso/java/aula36/Exercicio01.java
@@ -0,0 +1,22 @@
+package curso.java.aula36;
+
+public class Exercicio01 {
+    private String nome;
+   
+    
+
+    /**
+     * @return String return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
--- src/curso/java/aula36/Main.java
+++ src/curso/java/aula36/Main.java
@@ -1,13 +1,17 @@
 package curso.java.aula36;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] arg){
-        Contato contato = new Contato();
+    public static void main(String[] arg) {
+        Contato contato = new Contato();// --> objeto do tipo contato
         contato.setNome("Alencar");
-        //contato.setEndereco("Rua Antonio Joaquim de Freitas"); -->primeiro cria objeto endereco
+        // contato.setEndereco("Rua Antonio Joaquim de Freitas"); -->primeiro cria
+        // objeto endereco
         // contato.setTelefone("9999999999"); --> primeiro cria objeto telefone
         // criando objeto endereço
-        Endereco end = new Endereco();
+
+        Endereco end = new Endereco();// --> obejto do tipo endereço
         end.setNomeRua("Rua Antônio Joaquim de Freitas");
         end.setComplemento("casa");
         end.setCidade("Florianópolis");
@@ -15,7 +19,7 @@ public class Main {
         contato.setEndereco(end);
 
         // criando objeto telefone
-        Telefone tel = new Telefone();
+        Telefone tel = new Telefone();// --> objeto do tipo telefone
         tel.setTipo("Celular");
         tel.setDdd("48");
         tel.setNumero("999999999");
@@ -29,13 +33,20 @@ public class Main {
         Telefone[] telefones = new Telefone[2];
         telefones[0] = tel;
         telefones[1] = tel1;
-        //set o telefone no contato
+        // set o telefone no contato
         contato.setTelefones(telefones);
 
         System.out.println(contato.getNome());
-        System.out.println(contato.getEndereco().getNomeRua());
-            for(Telefone t : contato.getTelefones()) {
-                System.out.println(t.getDdd() + " " + t.getNumero());
-            }
+        if (contato.getEndereco() != null) {
+            System.out.println(contato.getEndereco().getCidade());
+            System.out.println(contato.getEndereco().getNomeRua());
+        }
+        for (Telefone t : contato.getTelefones()) {
+            System.out.println(t.getDdd() + " " + t.getNumero());
+        }
+
+        System.out.println("-----------------------------------------------------------------------Exercicio01");
+
+        Scanner scan = new Scanner(System.in);
     }
 }
