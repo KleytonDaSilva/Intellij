899a2b2a1ffddeb0c0f68d6530113fdd5df519d9
Kleyton <nksaddam@gmail.com>
2022-06-14 16:51:58 -0300

    Atualizado 14/06/2022  java

---
+++ src/curso/java/aula42/ClasseFinal.java
@@ -0,0 +1,15 @@
+package Intellij.src.curso.java.aula42;
+
+public class ClasseFinal {
+    public static void main(String[] args) {
+
+        Pessoal pessoa = new Pessoal();
+        // Pessoal aluno = new Aluno();
+        // Pessoal professor = new Professor();
+
+        Aluno aluno = new Aluno();
+        Professor professor = new Professor();
+
+    }
+
+}
